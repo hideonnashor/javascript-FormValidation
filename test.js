f84aecc91744abdea8ddef59bafe55611a5c2c9b
MineTea <30948800+MineTea@users.noreply.github.com>
2017-12-27 16:55:52 +0800

    局部变量修改

--- test.js
+++ test.js
@@ -5,15 +5,16 @@
  * @version $Id$
  */
  // 获取输入框的值
+
  function getValueLength(text){
- 	var text = document.getElementById(text);
- 	var content = text.value;
+ 	let text = document.getElementById(text);
+ 	let content = text.value;
  	return content;
  }
 // 字长检验
 function test1(){
 
-	var content = getValueLength("text01");
+	let content = getValueLength("text01");
 
 	if(content.length>20){
 		alert("超过限定长度");
@@ -21,8 +22,8 @@ function test1(){
 }
 // 中文检验
 function test2(){
-	var content = getValueLength("text02");
-	var reg = /^[\u0391-\uFFE5]+$/;
+	let content = getValueLength("text02");
+	let reg = /^[\u0391-\uFFE5]+$/;
 
 	if (!content.match(reg)||content==0) {
 		alert("必须为中文");
@@ -30,8 +31,8 @@ function test2(){
 }
 // 英文检验
 function test3(){
-var content = getValueLength("text03");
-	var reg = /^[A-z]+$/;
+	let content = getValueLength("text03");
+	let reg = /^[A-z]+$/;
 
 	if (!content.match(reg)||content==0) {
 		alert("必须为英文");
@@ -39,8 +40,8 @@ var content = getValueLength("text03");
 }
 // 数字检验
 function test4(){
-	var content = getValueLength("text04");
-	var reg = /^[0-9]+$/;
+	let content = getValueLength("text04");
+	let reg = /^[0-9]+$/;
 
 	if (!content.match(reg)||content==0) {
 		alert("必须为数字");
@@ -48,11 +49,11 @@ function test4(){
 }
 // 年份检验
 function test5(){
-	var content = getValueLength("text05");
-	var reg = /^[0-9]+$/;
+	let content = getValueLength("text05");
+	let reg = /^[0-9]+$/;
 
-	var date = new Date;
-	var year = date.getFullYear();
+	let date = new Date;
+	let year = date.getFullYear();
 
 	if (!content.match(reg)||content==0) {
 		alert("必须为大于0的年份");
@@ -63,7 +64,7 @@ function test5(){
 }
 // 星号检验
 function test6(){
-	var content = getValueLength("text06");
+	let content = getValueLength("text06");
 
 	if (content.indexOf("*")>-1) {
 		alert("不能输入星号");
@@ -71,8 +72,8 @@ function test6(){
 }
 // 密码检验
 function test7(){
-	var content1 = getValueLength("text07");
-	var content2 = getValueLength("text08");
+	let content1 = getValueLength("text07");
+	let content2 = getValueLength("text08");
 
 	if ((content1^content2)!=0) {
 		alert("两次密码不一致");
@@ -80,10 +81,11 @@ function test7(){
 }
 // 邮箱检验
 function test8(){
-	var content = getValueLength("text09");
-	var reg = /^[A-z0-9]+@[A-z0-9]+\.com$/;
+	let content = getValueLength("text09");
+	let reg = /^[A-z0-9]+@[A-z0-9]+\.com$/;
 
 	if (!2content.match(reg)||content==0) {
 		alert("请输入正确的国内邮箱");
 	}
 }
+
